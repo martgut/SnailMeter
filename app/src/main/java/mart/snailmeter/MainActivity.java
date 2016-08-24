7a684621ce77f0b6fc18417698b558d0075f4b48
mart <martin.gutschelhofer@gmx.at>
2016-08-24 14:45:34 +0200

    Initial commit

---
+++ app/src/main/java/mart/snailmeter/MainActivity.java
@@ -0,0 +1,21 @@
+package mart.snailmeter;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+    implements MeterFragment.OnFragmentInteractionListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
